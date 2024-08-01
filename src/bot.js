224bab5ec264d592f8644634906de406a61ce84b
Ahmed Rangel <ahmedrangel@outlook.com>
2024-08-01 00:55:08 -0500

    chore!: rework video handling and use as url embed instead of uploading

--- src/bot.js
+++ src/bot.js
@@ -241,12 +241,12 @@ router.post("/", async (req, env, context) => {
         */
         case C.VIDEO.name: {
           const followUpRequest = async () => {
-            const embeds = [], files = [], button = [], components = [];
+            const embeds = [], button = [], components = [];
             let emoji;
             let supported = false;
-            let red_social = "Instagram / Facebook / TikTok / Twitter / YouTube / Twitch / Kick";
+            let red_social = "Instagram / Facebook / TikTok / X / YouTube / Twitch / Kick";
             const url = getValue("link", options);
-
+        
             for (const key in VIDEO_SOCIALS) {
               const sns = VIDEO_SOCIALS[key];
               if (sns.domains.some(domains => url.includes(domains))) {
@@ -256,7 +256,7 @@ router.post("/", async (req, env, context) => {
                 break;
               }
             }
-
+        
             if (!esUrl(url) && !supported) {
               const error = `⚠️ Error. El texto ingresado no es un link válido de **${red_social}**`;
               return deferUpdate("", {
@@ -265,17 +265,14 @@ router.post("/", async (req, env, context) => {
                 embeds: errorEmbed(error)
               });
             }
-
+        
             const encodedUrl = encodeURIComponent(url);
             const scraperUrl = `https://dev.ahmedrangel.com/dc/${red_social.toLowerCase()}-video-scrapper`;
             const scraperQueries = { url: encodedUrl, filter: "video" };
             const scrapping = await $fetch(withQuery(scraperUrl, scraperQueries), { retry: 3, retryDelay: 1000 }).catch(() => null);
-            const video_url = scrapping?.video_url;
-            const short_url = scrapping?.short_url;
-            const status = scrapping?.status;
+            const { id, video_url, short_url, status } = scrapping;
             const caption = imbedUrlsFromString(`${scrapping?.caption ? scrapping?.caption?.replace(/#[^\s#]+(\s#[^\s#]+)*$/g, "").replaceAll(".\n", "").replace(/\n+/g, "\n").trim() : ""}`);
-            console.info(status);
-
+        
             if (status !== 200 && !esUrl(video_url)) {
               const error = ":x: Error. Ha ocurrido un error obteniendo el video.";
               return deferUpdate("", {
@@ -284,18 +281,46 @@ router.post("/", async (req, env, context) => {
                 embeds: errorEmbed(error)
               });
             }
-
+        
+            const finalReply = (downloadUrl) => {
+              button.push({
+                type: MessageComponentTypes.BUTTON,
+                style: ButtonStyleTypes.LINK,
+                label: "Descargar MP4",
+                url: downloadUrl
+              });
+        
+              components.push ({
+                type: MessageComponentTypes.ACTION_ROW,
+                components: button
+              });
+        
+              const mensaje = `${emoji} **${red_social}**: [${short_url.replace("https://", "")}](${withQuery(`https://dev.ahmedrangel.com/dc/fx`, { video_url: downloadUrl, redirect_url: short_url })})\n${caption}`;
+              const fixedMsg = mensaje.length > 1000 ? mensaje.substring(0, 1000) + "..." : mensaje;
+        
+              return deferUpdate(fixedMsg, {
+                token,
+                application_id: env.DISCORD_APPLICATION_ID,
+                embeds,
+                components
+              });
+            };
+        
+            const cdnUrl = `https://cdn.ahmedrangel.com/videos/${red_social.toLowerCase()}/${id}.mp4`;
+            const checkCdn = await $fetch.raw(cdnUrl).catch(() => null);
+            if (checkCdn?.ok) {
+              console.log("Existe en CDN");
+              return finalReply(cdnUrl);
+            }
+        
             const videoChecker = await $fetch.raw(video_url).catch(() => null);
             const blob = videoChecker?._data;
             const fileSize = blob?.size;
-            console.info("Tamaño: " + fileSize);
             const contentType = videoChecker?.headers.get("content-type");
-            console.info("Content-Type: " + contentType);
-
-            const guild = await getGuild(guild_id, env.DISCORD_TOKEN);
-            console.info("premium tier: " + guild.premium_tier);
-            const maxSize = guild.premium_tier >= 3 ? 100000000 : (guild.premium_tier === 2 ? 50000000 : 25000000);
-
+            console.log("Tamaño: " + fileSize, "Content-Type: " + contentType);
+        
+            const maxSize = 100000000;
+        
             if (blob && fileSize > maxSize) {
               const error = "⚠️ Error. El video es muy pesado o demasiado largo.";
               return deferUpdate("", {
@@ -304,7 +329,7 @@ router.post("/", async (req, env, context) => {
                 embeds: errorEmbed(error)
               });
             }
-
+        
             if (!blob || fileSize < 100 || !["video/mp4", "binary/octet-stream", "application/octet-stream"].includes(contentType)) {
               const error = ":x: Error. Ha ocurrido un error obteniendo el video.";
               return deferUpdate("", {
@@ -313,37 +338,9 @@ router.post("/", async (req, env, context) => {
                 embeds: errorEmbed(error)
               });
             }
-
-            const uploadedUrl = await $fetch(withQuery("https://dev.ahmedrangel.com/put/video", { url: video_url, bot_name: CONSTANTS.BOT }));
-            const uploadedId = obtenerIDDesdeURL(uploadedUrl);
-
-            files.push({
-              name: `${uploadedId}.mp4`,
-              file: blob
-            });
-
-            button.push({
-              type: MessageComponentTypes.BUTTON,
-              style: ButtonStyleTypes.LINK,
-              label: "Descargar MP4",
-              url: uploadedUrl
-            });
-
-            components.push ({
-              type: MessageComponentTypes.ACTION_ROW,
-              components: button
-            });
-
-            const mensaje = `${emoji} **${red_social}**: [${short_url.replace("https://", "")}](<${short_url}>)\n${caption}`;
-            const fixedMsg = mensaje.length > 1000 ? mensaje.substring(0, 1000) + "..." : mensaje;
-            // Return del refer
-            return deferUpdate(fixedMsg, {
-              token,
-              application_id: env.DISCORD_APPLICATION_ID,
-              embeds,
-              components,
-              files
-            });
+        
+            const uploadedUrl = await $fetch(withQuery(`https://dev.ahmedrangel.com/put/video`, { url: video_url, prefix: "videos", dir: red_social.toLowerCase(), file_id: id }));
+            return finalReply(uploadedUrl);
           };
           context.waitUntil(followUpRequest());
           return deferReply();
