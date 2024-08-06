91ea2f7396c4a2f1cb5a27bbba6f044cbc8c9a01
Yizack Rangel <yizackr@gmail.com>
2024-08-06 16:34:56 -0500

    fix: bot restrictions

--- server/api/webhooks.post.ts
+++ server/api/webhooks.post.ts
@@ -3,7 +3,7 @@ export default defineEventHandler(async (event) => {
   if (!isValidWebhook) throw createError({ statusCode: 401, message: "Unauthorized: webhook is not valid" });
 
   const body = await readBody<WebhookBody>(event);
-  const { type, data, channel_id } = body;
+  const { type, data } = body;
   if (type === InteractionType.PING) {
     console.info("Handling Ping request");
     return create(type);
@@ -23,17 +23,15 @@ export default defineEventHandler(async (event) => {
     [ANGAR.name]: handlerAngar // Comando /angar
   };
 
-  if (channel_id === CONSTANTS.CHANNEL || channel_id === CONSTANTS.CHANNEL_PRUEBAS) {
-    return create(type, () => {
-      const { name } = data;
-      const commandHandler = commandHandlers[name];
+  return create(type, () => {
+    const { name } = data;
+    const commandHandler = commandHandlers[name];
 
-      if (commandHandler) {
-        return commandHandler(event, body);
-      }
+    if (commandHandler) {
+      return commandHandler(event, body);
+    }
 
-      setResponseStatus(event, 404);
-      return { error: "Unknown Type" };
-    });
-  }
+    setResponseStatus(event, 404);
+    return { error: "Unknown Type" };
+  });
 });
