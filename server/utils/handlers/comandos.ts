9e8300577103ba07751dc45965b75914462c2160
Yizack Rangel <yizackr@gmail.com>
2024-08-13 01:03:43 -0500

    chore: remove command break lines

--- server/utils/handlers/comandos.ts
+++ server/utils/handlers/comandos.ts
@@ -1,13 +1,13 @@
 export const handlerComandos: CommandHandler = () => {
   const list: string[] = [];
   Object.values(COMMANDS).forEach((command) => {
-    list.push(`-  </${command.name}:${command.cid}> *${command.description}*\n\n`);
+    list.push(`-  </${command.name}:${command.cid}> *${command.description}*\n`);
   });
 
   return reply(null, {
     embeds: [{
       title: "Lista de comandos",
-      description: "Conoce la lista de comandos disponibles.\n"
+      description: "Conoce la lista de comandos disponibles.\n\n"
       + `${list.join("")}`
       + "Escribe el comando que desees en la caja de enviar mensajes de discord y selecciona la opción que se muestra junto al avatar del bot. Se irán añadiendo más comandos divertidos con el tiempo.",
       color: CONSTANTS.COLOR,
