923117f451a6048e841c9fbc8e813106f8929e33
Yizack Rangel <yizackr@gmail.com>
2024-08-13 00:44:29 -0500

    chore: H3Event type not needed for auto export

--- server/utils/exports.ts
+++ server/utils/exports.ts
@@ -1,7 +1,6 @@
 export { SITE } from "~/utils/site-info";
 export { hash } from "ohash";
 export { withQuery } from "ufo";
-export type { H3Event } from "h3";
 export { verifyKey, ButtonStyleTypes, MessageComponentTypes, InteractionType } from "discord-interactions";
 export { z } from "zod";
 export * as COMMANDS from "./commands";
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,5 @@
+import type { H3Event } from "h3";
+
 export {};
 
 declare global {
