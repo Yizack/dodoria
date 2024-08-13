9c9c7f92e7c9ca028f6350bf58418a9824082872
Yizack Rangel <yizackr@gmail.com>
2024-08-13 00:13:45 -0500

    fix: ship

--- server/utils/helpers.ts
+++ server/utils/helpers.ts
@@ -1,39 +1,97 @@
-import { SITE as siteInfo } from "~/utils/site-info";
+export const getValue = (name: string, options: Record<string, string>[] | null) => {
+  if (!options) return "";
+  const option = options.find(option => option.name === name);
+  return option?.value ?? "";
+};
 
-export { hash } from "ohash";
-export { withQuery } from "ufo";
-export type { H3Event } from "h3";
-export { verifyKey, ButtonStyleTypes, MessageComponentTypes, InteractionType } from "discord-interactions";
-export { z } from "zod";
+export const getRandom = (options: { min?: number, max: number }) => {
+  const min = options.min ?? 1;
+  return Math.round((Math.random() * (options.max - min)) + min);
+};
 
-export const SITE = siteInfo;
+export const getRandomAngar = () => {
+  const number = getRandom({ min: 1, max: CONSTANTS.ANGAR });
+  return `https://dodoria.yizack.com/images/angar/${number}.jpg`;
+};
 
-export const decodeCode = (encodedCode: string) => {
-  try {
-    const decodedString = atob(encodedCode);
-    const params = JSON.parse(decodedString);
+export const getRandomBuenoGente = () => {
+  const number = getRandom({ min: 1, max: CONSTANTS.BUENOGENTE });
+  return `https://dodoria.yizack.com/images/buenogente/${number}.jpg`;
+};
 
-    const paramsSchema = z.object({
-      p: z.number({ coerce: true }).int().min(0).max(100),
-      u: z.array(z.bigint()).length(2),
-      a: z.array(z.string().nullable()).length(2),
-      d: z.array(z.number()).length(2)
-    });
+export const esUrl = (cadena: string) => {
+  const regex = /^(?:https?:\/\/)?(?:www\.)?([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,})(?:\/\S*)?$/;
+  return regex.test(cadena);
+};
 
-    const decodedParams = {
-      p: params.p,
-      u: params.u.map(BigInt),
-      a: params.a,
-      d: params.d.map(Number)
-    };
+export const imbedUrlsFromString = (str: string) => {
+  const regex = /(https?:\/\/[^\s]+)/g;
+  return str.replace(regex, "<$1>");
+};
 
-    const result = paramsSchema.safeParse(decodedParams);
-
-    if (!result.success) throw createError({ statusCode: 400, message: result.error.message });
-    return result.data;
+export const obtenerIDDesdeURL = (url: string) => {
+  const expresionRegular = /\/([a-zA-Z0-9_-]+)(?:\.[a-zA-Z0-9]+)?(?:\?|$|\/\?|\/$)/;
+  const resultado = expresionRegular.exec(url);
+  if (resultado && resultado.length > 1) {
+    return resultado[1];
   }
-  catch (error) {
-    console.warn(error);
-    throw createError({ statusCode: 400, message: "Invalid image code" });
+  else {
+    return null;
   }
+};
+
+export const errorEmbed = (error_msg: string) => {
+  const embeds = [];
+  embeds.push({
+    color: CONSTANTS.COLOR,
+    description: error_msg
+  });
+  return embeds;
+};
+
+export const getRandomAngarMessage = () => {
+  const messages = [
+    "Te presento el ANGAR que te representa en este momento.",
+    "Hoy estás en este mood, y el ANGAR lo refleja perfectamente.",
+    "Este es el ANGAR que captura tu esencia hoy.",
+    "El ANGAR de hoy refleja tu estado de ánimo actual.",
+    "Descubre el ANGAR que encaja con tu vibra de hoy.",
+    "Este ANGAR coincide con tu energía en este día.",
+    "El ANGAR de hoy está en sintonía con tu estado de ánimo.",
+    "Hoy te presento el ANGAR que mejor representa tu vibe.",
+    "Este ANGAR en particular se alinea con tu estado de ánimo actual.",
+    "¡Descubre el ANGAR que te acompaña en este momento!",
+    "¿Listo para conocer el ANGAR interior que ni siquiera sabías que existía?",
+    "Prepárate para descubrir el ANGAR que hace temblar a los demás, o al menos eso esperamos.",
+    "Este es el ANGAR que lo sabe todo, ¡disfrútalo!",
+    "El ANGAR de hoy podría asustar a tus amigos, ¡así que úsalo con precaución!",
+    "Descubre el ANGAR que hace que hasta los high elo se sientan inseguros.",
+    "Este ANGAR es tan grande que ni siquiera cabe en esta frase.",
+    "El ANGAR de hoy es como un tornado de autoestima, ¡cuidado con no volar lejos!",
+    "Hoy te presento el ANGAR que hará que todos los narcisistas se sientan humildes.",
+    "¡Este ANGAR es tan grande que debería tener su propio programa de televisión!",
+    "¿Estás listo para conocer el terror de pacasmayo?",
+    "Este es el ANGAR que podría hacer llorar a cualquiera de envidia.",
+    "El ANGAR de hoy podría causar estragos en la autoestima de cualquiera, ¡incluido tú!",
+    "Descubre el ANGAR que incluso a Shakespeare le costaría describir con palabras.",
+    "Hoy te presento el ANGAR que hace que los egos de Hollywood se encojan de envidia.",
+    "Este ANGAR es tan grande que su sombra eclipsa hasta al sol.",
+    "¡Este ANGAR es tan épico que merece un monumento!",
+    "El ANGAR de hoy es como un tsunami de confianza, ¡prepárate para ser arrastrado por él!",
+    "Este es el ANGAR que podría hacer que los leones se sientan como gatitos en comparación.",
+    "El ANGAR de hoy es tan brillante que podría iluminar hasta la noche más oscura.",
+    "¡Este ANGAR es tan magnífico que merece su propio día festivo!",
+    "Este es el ANGAR que podría hacer que los dioses griegos se sientan inseguros.",
+    "El ANGAR de hoy es tan grande que debería tener su propia gravedad.",
+    "Hoy te presento el ANGAR que podría ser presidente.",
+    "Este ANGAR es tan monumental que debería estar en los libros de historia.",
+    "¡Este ANGAR es tan legendario que merece su propia saga!",
+    "Este es el ANGAR que podría hacer que los gigantes se sientan pequeños.",
+    "Este ANGAR es tan influyente que debería tener su propio ejército de seguidores.",
+    "¡Este ANGAR es tan impresionante que merece su propio emoji!",
+    "Hoy te presento el ANGAR que hace que los millonarios se sientan humildes.",
+    "Este ANGAR es tan monumental que incluso las pirámides se sienten pequeñas a su lado.",
+    "¡Este ANGAR es tan épico que merece ser esculpido en una montaña!"
+  ];
+  return messages[getRandom({ min: 0, max: messages.length - 1 })];
 };
--- server/utils/ship.ts
+++ server/utils/ship.ts
@@ -110,3 +110,33 @@ export const getShipImage = async (data: {
     return;
   }
 };
+
+export const decodeCode = (encodedCode: string) => {
+  try {
+    const decodedString = atob(encodedCode);
+    const params = JSON.parse(decodedString);
+
+    const paramsSchema = z.object({
+      p: z.number({ coerce: true }).int().min(0).max(100),
+      u: z.array(z.bigint()).length(2),
+      a: z.array(z.string().nullable()).length(2),
+      d: z.array(z.number()).length(2)
+    });
+
+    const decodedParams = {
+      p: params.p,
+      u: params.u.map(BigInt),
+      a: params.a,
+      d: params.d.map(Number)
+    };
+
+    const result = paramsSchema.safeParse(decodedParams);
+
+    if (!result.success) throw createError({ statusCode: 400, message: result.error.message });
+    return result.data;
+  }
+  catch (error) {
+    console.warn(error);
+    throw createError({ statusCode: 400, message: "Invalid image code" });
+  }
+};
