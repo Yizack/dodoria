df8aab141fb57807b2bc01eed11d79f67724f133
Yizack Rangel <yizackr@gmail.com>
2024-08-15 06:16:21 -0500

    chore: avatarGlobal prop

--- server/utils/handlers/avatar.ts
+++ server/utils/handlers/avatar.ts
@@ -6,11 +6,13 @@ export const handlerAvatar: CommandHandler = (event, { body, getValue }) => {
   const user = resolved && userId ? {
     id: userId,
     username: resolved.users[userId]?.username,
+    avatarGlobal: resolved.users[userId]?.avatar,
     avatar: avatarType === "servidor" && resolved.members[userId]?.avatar ? resolved.members[userId]?.avatar : resolved.users[userId]?.avatar,
     discriminator: resolved.users[userId]?.discriminator
   } : {
     id: body.member.user.id,
     username: body.member.user.username,
+    avatarGlobal: body.member.user.avatar,
     avatar: avatarType === "servidor" && body.member.avatar ? body.member.avatar : body.member.user.avatar,
     discriminator: body.member.user.discriminator
   };
@@ -19,7 +21,7 @@ export const handlerAvatar: CommandHandler = (event, { body, getValue }) => {
     type: MessageComponentTypes.BUTTON,
     style: ButtonStyleTypes.LINK,
     label: "Global Avatar",
-    url: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`
+    url: getAvatarURL(user.id, user.avatarGlobal, user.discriminator, 1024)
   }];
 
   const components = [{
