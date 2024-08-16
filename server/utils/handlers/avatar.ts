809ed6927a16b2a5997f2430911cb89a25e8a9c3
Yizack Rangel <yizackr@gmail.com>
2024-08-16 00:42:52 -0500

    chore: improve avatar code

--- server/utils/handlers/avatar.ts
+++ server/utils/handlers/avatar.ts
@@ -4,92 +4,54 @@ export const handlerAvatar: CommandHandler = (event, { body, getValue }) => {
   const userId = getValue("usuario");
   const avatarType = getValue("tipo") || "servidor";
 
-  interface DiscordAvatarInfo {
-    id: string;
-    username?: string;
-    avatarGlobal?: string | null;
-    avatar?: string | null;
-    discriminator?: string;
-    guildId?: string;
+  const getUserInfo = (sourceUser: DiscordUser, sourceMember: Omit<DiscordMember, "user"> | null) => ({
+    id: sourceUser.id,
+    username: sourceUser.username,
+    avatarGlobal: sourceUser.avatar,
+    avatar: avatarType === "servidor" && sourceMember?.avatar ? sourceMember.avatar : sourceUser.avatar,
+    discriminator: sourceUser.discriminator,
+    guildId: avatarType === "servidor" && sourceMember?.avatar ? body.guild_id : undefined
+  });
+
+  let userInfo: ReturnType<typeof getUserInfo>;
+
+  if (resolved && userId) userInfo = getUserInfo(resolved.users[userId], resolved.members[userId]);
+  else if (context === 0) userInfo = getUserInfo(member.user, member);
+  else userInfo = getUserInfo(user, null);
+
+  let components: DiscordComponent[] | undefined;
+
+  if (context === 0) {
+    const button = [{
+      type: MessageComponentTypes.BUTTON,
+      style: ButtonStyleTypes.LINK,
+      label: "Global Avatar",
+      url: getAvatarURL({
+        userId: userInfo.id,
+        avatarHash: userInfo.avatarGlobal,
+        userDiscriminator: userInfo.discriminator,
+        imageSize: 1024
+      })
+    }];
+
+    components = [{
+      type: MessageComponentTypes.ACTION_ROW,
+      components: button
+    }];
   }
-
-  const info: { user: DiscordAvatarInfo } = {
-    user: {
-      id: "",
-      username: "",
-      avatarGlobal: "",
-      avatar: "",
-      discriminator: "",
-      guildId: ""
-    }
-  };
-
-  switch (context) {
-    case 0:
-      info.user = resolved && userId ? {
-        id: userId,
-        username: resolved.users[userId]?.username,
-        avatarGlobal: resolved.users[userId]?.avatar,
-        avatar: avatarType === "servidor" && resolved.members[userId]?.avatar ? resolved.members[userId]?.avatar : resolved.users[userId]?.avatar,
-        discriminator: resolved.users[userId]?.discriminator,
-        guildId: avatarType === "servidor" && resolved.members[userId]?.avatar ? body.guild_id : undefined
-      } : {
-        id: member.user.id,
-        username: member.user.username,
-        avatarGlobal: member.user.avatar,
-        avatar: avatarType === "servidor" && member.avatar ? member.avatar : member.user.avatar,
-        discriminator: member.user.discriminator,
-        guildId: avatarType === "servidor" && member.avatar ? body.guild_id : undefined
-      };
-      break;
-    case 1:
-    case 2:
-      console.info(body);
-      info.user = resolved && userId ? {
-        id: userId,
-        username: resolved.users[userId]?.username,
-        avatarGlobal: resolved.users[userId]?.avatar,
-        avatar: resolved.users[userId]?.avatar,
-        discriminator: resolved.users[userId]?.discriminator
-      } : {
-        id: user.id,
-        username: user.username,
-        avatarGlobal: user.avatar,
-        avatar: user.avatar,
-        discriminator: user.discriminator
-      };
-      break;
-  }
-
-  const button = [{
-    type: MessageComponentTypes.BUTTON,
-    style: ButtonStyleTypes.LINK,
-    label: "Global Avatar",
-    url: getAvatarURL({
-      userId: info.user.id,
-      avatarHash: info.user.avatarGlobal,
-      userDiscriminator: info.user.discriminator,
-      imageSize: 1024
-    })
-  }];
-
-  const components = [{
-    type: MessageComponentTypes.ACTION_ROW,
-    components: button
-  }];
 
   return reply(null, {
-    components: context === 0 ? components : undefined,
+    components,
     embeds: [{
-      title: info.user.username + (Number(info.user.discriminator) ? `#${info.user.discriminator}` : ""),
+      title: userInfo.username + (Number(userInfo.discriminator) ? `#${userInfo.discriminator}` : ""),
       color: CONSTANTS.COLOR,
       image: {
         url: getAvatarURL({
-          userId: info.user.id,
-          avatarHash: info.user.avatar,
-          userDiscriminator: info.user.discriminator,
+          userId: userInfo.id,
+          avatarHash: userInfo.avatar,
+          userDiscriminator: userInfo.discriminator,
           imageSize: 1024,
-          guildId: info.user.guildId
+          guildId: userInfo.guildId
         })
       }
     }]
