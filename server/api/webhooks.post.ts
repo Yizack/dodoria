34f71ca1226f45d42e8cd9da4097d13d4d78c94e
Ahmed Rangel <ahmedrangel@outlook.com>
2024-10-05 23:15:55 -0500

    fix: handler

--- server/api/webhooks.post.ts
+++ server/api/webhooks.post.ts
@@ -12,7 +12,7 @@ export default defineEventHandler(async (event) => {
   if (type === InteractionType.MESSAGE_COMPONENT) {
     console.info("Handling Message Component request");
     switch (data?.custom_id) {
-      case "btn_reload": return handlerVideoReload;
+      case "btn_reload": return handlerVideoReload(event, { body });
     }
   }
 
