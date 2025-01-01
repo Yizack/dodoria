c92dfcdd083fdfc7281bbb7359e65af011127c78
Yizack Rangel <yizackr@gmail.com>
2025-01-01 15:06:22 -0500

    fix: types

--- types/commands.d.ts
+++ types/commands.d.ts
@@ -3,7 +3,9 @@ import type { APIApplicationCommand } from "discord-api-types/v10";
 export {};
 
 declare global {
-  interface Command extends Partial<APIApplicationCommand> {
+  type CommandPartialProperties = "id" | "type" | "application_id" | "default_member_permissions" | "version";
+  type ApplicationCommand = Omit<APIApplicationCommand, CommandPartialProperties> & Partial<Pick<APIApplicationCommand, CommandPartialProperties>>;
+  interface Command extends ApplicationCommand {
     cid: string;
   }
 }
