cc2ebdeb2cf8f2d94243d08948ea4ff3e127b366
Yizack Rangel <yizackr@gmail.com>
2025-01-01 16:07:31 -0500

    chore: add estática

--- shared/utils/copys-list.ts
+++ shared/utils/copys-list.ts
@@ -4,5 +4,6 @@ export const COPYS_LIST = [
   { name: "Cale a boca, seu serrano de merda", value: "n_7ARLQGEMK3:25" },
   { name: "El caballero posee la túnica verde", value: "n_HGCQ9NT8OC:27" },
   { name: "Dile puta a mi mamita, no te preocupes", value: "n_I7XVFSR61E:14" },
-  { name: "Fuera de bromas me sobra el dinero", value: "n_0R2XWBXPKW:14" }
+  { name: "Fuera de bromas me sobra el dinero", value: "n_0R2XWBXPKW:14" },
+  { name: "ESTÁTICA", value: "n_E2F8PGAIXW:11" }
 ].sort((a, b) => a.name.localeCompare(b.name));
