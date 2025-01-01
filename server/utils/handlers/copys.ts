36f9d2c92f4babcb34b0f8e91042ef77833b81d3
Yizack Rangel <yizackr@gmail.com>
2025-01-01 04:48:32 -0500

    fix: no uploaded filename needed

--- server/utils/handlers/copys.ts
+++ server/utils/handlers/copys.ts
@@ -17,7 +17,6 @@ export const handlerCopys: CommandHandler = (event, { body, getValue }) => {
         {
           id: 0,
           filename: filename,
-          uploaded_filename: filename,
           duration_secs: 10,
           waveform: "acU6Va9UcSVZzsVw7IU/80s0Kh/pbrTcwmpR9da4mvQejIMykkgo9F2FfeCd235K/atHZtSAmxKeTUgKxAdNVO8PAoZq1cHNQXT/PHthL2sfPZGSdxNgLH0AuJwVeI7QZJ02ke40+HkUcBoDdqGDZeUvPqoIRbE23Kr+sexYYe4dVq+zyCe3ci/6zkMWbVBpCjq8D8ZZEFo/lmPJTkgjwqnqHuf6XT4mJyLNphQjvFH9aRqIZpPoQz1sGwAY2vssQ5mTy5J5muGo+n82b0xFROZwsJpumDsFi4Da/85uWS/YzjY5BdxGac8rgUqm9IKh7E6GHzOGOy0LQIz3O4ntTg=="
         }
