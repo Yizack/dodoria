d2c0caaa23636cd4f77d9863019bbdf877630f46
Yizack Rangel <yizackr@gmail.com>
2025-01-01 03:38:39 -0500

    fix: add waveform

--- server/utils/handlers/copys.ts
+++ server/utils/handlers/copys.ts
@@ -7,7 +7,7 @@ export const handlerCopys: CommandHandler = (event, { body, getValue }) => {
   const followUpRequest = async () => {
     // TODO: handle when not found
     const blob = await hubBlob().get(`copys/${filename}`);
-    const files = [{ name: `${audioId}.ogg`, file: blob }];
+    const files = [{ name: filename, file: blob }];
     return deferUpdate("", {
       flags: 8192,
       token,
@@ -15,11 +15,11 @@ export const handlerCopys: CommandHandler = (event, { body, getValue }) => {
       files,
       attachments: [
         {
-          id: "0",
+          id: 0,
           filename: filename,
           uploaded_filename: filename,
-          duration_secs: 10
-          // waveform: "<base64 encoded byte array>"
+          duration_secs: 10,
+          waveform: "acU6Va9UcSVZzsVw7IU/80s0Kh/pbrTcwmpR9da4mvQejIMykkgo9F2FfeCd235K/atHZtSAmxKeTUgKxAdNVO8PAoZq1cHNQXT/PHthL2sfPZGSdxNgLH0AuJwVeI7QZJ02ke40+HkUcBoDdqGDZeUvPqoIRbE23Kr+sexYYe4dVq+zyCe3ci/6zkMWbVBpCjq8D8ZZEFo/lmPJTkgjwqnqHuf6XT4mJyLNphQjvFH9aRqIZpPoQz1sGwAY2vssQ5mTy5J5muGo+n82b0xFROZwsJpumDsFi4Da/85uWS/YzjY5BdxGac8rgUqm9IKh7E6GHzOGOy0LQIz3O4ntTg=="
         }
       ]
     });
