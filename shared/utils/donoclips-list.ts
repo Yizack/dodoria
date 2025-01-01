e3bb8833b3a05844e88d0b56fb5a6ffe4955848c
Yizack Rangel <yizackr@gmail.com>
2025-01-01 18:56:39 -0500

    feat: add more donoclips

--- shared/utils/donoclips-list.ts
+++ shared/utils/donoclips-list.ts
@@ -1,4 +1,14 @@
 export const DONOCLIPS_LIST = [
-  { name: "Cállate plopiii pesado", value: "78262ba0-aedd-47fd-bb5f-8480febb5865:10" },
-  { name: "Cuando se sientan mal...", value: "f884842c-5d39-4cfd-8283-bd7c044a474b:10" }
+  { name: "Cállate plopiii pesado", value: "78262ba0-aedd-47fd-bb5f-8480febb5865:9" },
+  { name: "Cuando se sientan mal...", value: "f884842c-5d39-4cfd-8283-bd7c044a474b:10" },
+  { name: "Ya todos sabemos que usas vots", value: "cb118b6d-c01b-4d31-8e7f-9616dd9ab32a:9" },
+  { name: "Video IA", value: "2102d97b-ac55-48e7-8f8a-822eeace896c:10" },
+  { name: "Soy tu puto jefe", value: "9cc70100-c0bf-4f23-9d3c-296c61934afe:6" },
+  { name: "Soy tu puto jefe 3", value: "5d76dc40-faad-44c5-b71c-5b2edd1a16c1:7" },
+  { name: "Soy tu puto jefe 2", value: "4e1a44e1-9a84-443e-bb6d-8dc7cbf77ca0:8" },
+  { name: "Lo único que encontramos son estafas", value: "d795c348-6103-4fdb-9620-a3d3d97a2bb8:10" },
+  { name: "Como habla webadas", value: "042f5f1d-8a57-4899-8a76-e66da54a3a04:6" },
+  { name: "Baneame Botanicuck", value: "f45780e5-260f-4070-aa2a-d485696b2e65:9" },
+  { name: "Alguien se suscribirá", value: "ddae6866-5577-4873-aadb-0c505ee372c6:10" },
+  { name: "Acéptalo y ya cacha gorditas", value: "7fbea18d-785c-4590-a752-414e8d7c8d98:8" }
 ].sort((a, b) => a.name.localeCompare(b.name));
