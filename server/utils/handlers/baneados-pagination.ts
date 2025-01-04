98ac4ccd6d0e78f8042be472270414911e4a838e
Ahmed Rangel <ahmedrangel@outlook.com>
2025-01-04 05:30:07 -0500

    fix: typecheck

--- server/utils/handlers/baneados-pagination.ts
+++ server/utils/handlers/baneados-pagination.ts
@@ -8,7 +8,7 @@ export const handlerBaneadosPagination: ComponentHandler = (event, { body }) => 
     const [current, available] = pages as string[];
     const cacheKey = `fn:baneados:${message.interaction!.id}.json`;
     const baneados = await useStorage("cache").getItem<{ value: BaneadoEntry[] }>(cacheKey);
-    const buttons = message.components[0].components;
+    const buttons = message.components[0]!.components;
     if (!baneados) {
       for (const b of buttons) b.disabled = true;
       const components = [{
@@ -23,7 +23,7 @@ export const handlerBaneadosPagination: ComponentHandler = (event, { body }) => 
       });
     }
 
-    const newCurrent = custom_id === "btn_baneados_prev" ? Number(current) - 1 : Number(current + 1);
+    const newCurrent = custom_id === "btn_baneados_prev" ? Number(current!) - 1 : Number(current! + 1);
     for (const b of buttons) {
       if ("btn_baneados_prev" === b.custom_id && (newCurrent <= 1)) b.disabled = true;
       else if ("btn_baneados_next" === b.custom_id && (newCurrent >= Number(available))) b.disabled = true;
