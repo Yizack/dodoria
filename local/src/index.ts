a1a99595751c68c80a3d42903b4db4d4191c6fad
Ahmed Rangel <ahmedrangel@outlook.com>
2025-01-06 20:00:10 -0500

    chore(local): also send banned user message to kick

--- local/src/index.ts
+++ local/src/index.ts
@@ -162,7 +162,8 @@ Kick.client.on(Kick.Events.Chatroom.UserBanned, async (event) => {
     const formattedDuration = fixedDuration ? formatDuration(fixedDuration, { format: ["days", "hours", "minutes"], locale: es }) : null;
     const messageHelper = isBanned ? "ha sido baneado permanentemente" : `ha recibido un timeout de ${formattedDuration}`;
     console.info(`${data.user.username} ${messageHelper} por ${data.banned_by.username}`);
-    channel.send(`## <:kick:1267449535668555788> \`${data.user.username}\` ${messageHelper}. <:pepoPoint:712364175967518730>`);
+    await Kick.client.api.chat.sendMessage(kickChannel.chatroomId, `@${data.user.username} ${messageHelper}`).catch(() => null);
+    await channel.send(`## <:kick:1267449535668555788> \`${data.user.username}\` ${messageHelper}. <:pepoPoint:712364175967518730>`);
   }
   catch (error) {
     console.info("Error al enviar el mensaje a Discord:", error);
