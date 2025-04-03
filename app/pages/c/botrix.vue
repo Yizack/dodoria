eaf354045ce233dbce98d4049fb33c0e461f630e
Yizack Rangel <yizackr@gmail.com>
2025-04-03 14:05:20 -0500

    chore: localize points

--- app/pages/c/botrix.vue
+++ app/pages/c/botrix.vue
@@ -33,7 +33,7 @@ const formatDate = (date: string) => new Date(date).toLocaleDateString("es-MX", 
               <tr>
                 <th scope="row">{{ i + 1 }}</th>
                 <td><NuxtLink :to="`https://kick.com/${user.name}`" external target="_blank">{{ user.name }}</NuxtLink></td>
-                <td>{{ user.points }}</td>
+                <td>{{ user.points.toLocaleString() }}</td>
                 <td>{{ user.watchtime }}</td>
                 <td>{{ user.followage?.date ? formatDate(user.followage.date) : '' }}</td>
               </tr>
