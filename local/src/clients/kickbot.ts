586dcf01b02e67f07393c8806e32809625bbde49
Yizack Rangel <yizackr@gmail.com>
2025-04-09 19:18:16 -0500

    fix(kickbot): fix client not open

--- local/src/clients/kickbot.ts
+++ local/src/clients/kickbot.ts
@@ -3,14 +3,13 @@ import { WebSocket } from "ws";
 const socketURL = "wss://kickbot.live/ws";
 let client = new WebSocket(socketURL);
 
-client.once("open", () => {
-  console.info("Connected to KickBot server");
-});
-
 const subscribe = (channelId: number) => {
-  const info = JSON.stringify({ type: "subscribe", channel: `kb${channelId}` });
-  client.send(info);
-  console.info("Subscribed to KickBot channel events");
+  client.once("open", () => {
+    console.info("Connected to KickBot server");
+    const info = JSON.stringify({ type: "subscribe", channel: `kb${channelId}` });
+    client.send(info);
+    console.info("Subscribed to KickBot channel events");
+  });
 };
 
 const reconnect = (channelId: number) => {
