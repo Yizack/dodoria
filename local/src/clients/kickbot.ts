1f06e1b7e58a268772b96fbdc81d2a11861dca68
Yizack Rangel <yizackr@gmail.com>
2025-04-16 20:12:08 -0500

    fix(kickbot): wait for connection before subscribe

--- local/src/clients/kickbot.ts
+++ local/src/clients/kickbot.ts
@@ -3,12 +3,37 @@ import { WebSocket } from "ws";
 const socketURL = "wss://kickbot.live/ws";
 let client = new WebSocket(socketURL);
 
+client.once("open", () => {
+  console.info("Connected to KickBot server");
+});
+
+const waitForOpenConnection = (socket: WebSocket) => {
+  if (socket.readyState === WebSocket.OPEN) return Promise.resolve();
+  return new Promise<void>((resolve, reject) => {
+    function onOpen () {
+      cleanup();
+      resolve();
+    }
+    function onError (err: WebSocket.ErrorEvent) {
+      cleanup();
+      reject(err);
+    }
+    function cleanup () {
+      socket.removeEventListener("open", onOpen);
+      socket.removeEventListener("error", onError);
+    }
+    socket.addEventListener("open", onOpen);
+    socket.addEventListener("error", onError);
+  });
+};
+
 const subscribe = (channelId: number) => {
-  client.once("open", () => {
-    console.info("Connected to KickBot server");
+  waitForOpenConnection(client).then(() => {
     const info = JSON.stringify({ type: "subscribe", channel: `kb${channelId}` });
     client.send(info);
     console.info("Subscribed to KickBot channel events");
+  }).catch((err) => {
+    console.warn("Error subscribing to KickBot channel events:", err);
   });
 };
 
