72065d3ce6a79ad48f28d75bcb317998a20610da
Yizack Rangel <yizackr@gmail.com>
2025-05-12 23:33:58 -0500

    fix(local): fix missing `$fetch` import for D1 query

--- local/src/utils/database.ts
+++ local/src/utils/database.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/d1";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
+import { $fetch } from "ofetch";
 import { useLocalConfig } from "./config";
 
 export { eq, and, or, desc, count, inArray, sql } from "drizzle-orm";
