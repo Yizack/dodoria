f36740f6ba7a4348d3fb25c6cbb9fd7233689ff9
Yizack Rangel <yizackr@gmail.com>
2025-05-24 02:10:38 -0500

    refactor: no need to throw error

--- server/api/baneados/kick.ts
+++ server/api/baneados/kick.ts
@@ -1,9 +1,7 @@
 export default defineEventHandler(async () => {
   const DB = useDB();
   const entries = await DB.select().from(tables.kickBans).orderBy(desc(tables.kickBans.createdAt)).all();
-  if (!entries.length) {
-    throw new Error("No hay logs para mostrar.");
-  }
+
   return entries.map(entry => ({
     id: entry.id,
     username: entry.username,
