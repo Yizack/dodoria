f8b35b5ebfe3681bcf2941501db28f8f382a95ed
Ahmed Rangel <ahmedrangel@outlook.com>
2025-06-05 17:47:57 -0500

    chore: temporary disable kick webhook validator

--- server/api/webhooks/kick.post.ts
+++ server/api/webhooks/kick.post.ts
@@ -1,8 +1,7 @@
 export default defineEventHandler(async (event) => {
-  const isValidWebhook = await isValidKickWebhook(event);
-  if (!isValidWebhook) throw createError({ statusCode: 401, message: "Unauthorized: webhook is not valid" });
+  // const isValidWebhook = await isValidKickWebhook(event);
+  // if (!isValidWebhook) throw createError({ statusCode: 401, message: "Unauthorized: webhook is not valid" });
 
   const body = await readBody<KickWebhookBody>(event);
-
   return handleKickWebhook(event, body);
 });
