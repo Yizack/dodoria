422a5f0597e219c3cd48555848740649000f82bd
Yizack Rangel <yizackr@gmail.com>
2025-07-05 13:31:40 -0500

    refactor(register): refactor

--- scripts/register.ts
+++ scripts/register.ts
@@ -6,10 +6,11 @@ import * as COMMANDS from "../shared/utils/commands";
 
 process.loadEnvFile();
 
-const rest = new REST({ version: "10" }).setToken(process.env.NUXT_DISCORD_TOKEN!);
 const commandsArray = Object.values(COMMANDS);
+const { NUXT_DISCORD_TOKEN, NUXT_DISCORD_APPLICATION_ID } = process.env;
+
+const rest = new REST({ version: "10" }).setToken(NUXT_DISCORD_TOKEN!);
 
 console.info("Started refreshing application (/) commands.");
-await rest.put(Routes.applicationCommands(process.env.NUXT_DISCORD_APPLICATION_ID!), { body: commandsArray });
-
+await rest.put(Routes.applicationCommands(NUXT_DISCORD_APPLICATION_ID!), { body: commandsArray });
 console.info("Successfully reloaded application (/) commands.");
